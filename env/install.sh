c4689fbbaa5766e086d31ffd5d8f01b82760561d
Shashank Sharma <s21j@pm.me>
2022-02-18 19:02:09 +0100

    upd

---
+++ env/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update; \
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update && \
+sudo apt-get install -y aspnetcore-runtime-5.0
+sudo apt-get install dotnet-sdk-5.0
+
+
