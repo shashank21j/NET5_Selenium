0fd2ada2baa9e851027531948b34489bc63b428b
Shashank Sharma <s21j-login@protonmail.com>
2022-02-18 23:55:24 +0530

    Update install.sh

--- env/install.sh
+++ env/install.sh
@@ -6,6 +6,4 @@ sudo apt-get update; \
 sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
 sudo apt-get install -y aspnetcore-runtime-5.0
-sudo apt-get install dotnet-sdk-5.0
-
-
+sudo apt-get install -y dotnet-sdk-5.0
